2ec87d0b9ac80d497389be7c64918cd250e9ba79
Fernando Nogueira <abert036@uottawa.ca>
2025-05-19 15:23:26 -0400

    Add S3 configuration and integration

---
+++ src/lib/server/s3/index.ts
@@ -0,0 +1,23 @@
+import { S3Client } from 'bun';
+
+const {
+	S3_ENDPOINT,
+	S3_ACCESS_KEY_ID,
+	S3_SECRET_ACCESS_KEY,
+	S3_REGION,
+	S3_BUCKET,
+} = process.env;
+
+if (!S3_ENDPOINT || !S3_ACCESS_KEY_ID || !S3_SECRET_ACCESS_KEY || !S3_REGION || !S3_BUCKET) {
+	throw new Error("S3 environment variables are missing");
+}
+
+const s3 = new S3Client({
+	endpoint: S3_ENDPOINT,
+	accessKeyId: S3_ACCESS_KEY_ID,
+	secretAccessKey: S3_SECRET_ACCESS_KEY,
+	region: S3_REGION,
+	bucket: S3_BUCKET,
+});
+
+export default s3;
